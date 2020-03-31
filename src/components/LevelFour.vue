0a07c13524237915a502f75f55a8b000dff3a380
Luke Joseph <luke@trackwire.io>
2020-03-31 11:32:05 +0200

    add level four

---
+++ src/components/LevelFour.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="lava-out" class="border pb-12" @mouseover="trackMouse($event)">
+    <h1 class="text-4xl mb-12 text-center">Level {{ level }}</h1>
+
+  <div id="lava" class="flex flex-col mx-auto" style="width:300px;">
+    <div class="flex" style="height:130px;">
+      <div id="progress-22" class="bg-blue-400" style="width:20px; height:130px;"></div>
+      <div id="progress-23" class="bg-black" style="width:75px; height:20px;"></div>
+      <div id="progress-25" class="bg-black h-full" style="width:110px; height:20px;"></div>
+      <div id="progress-1" class="bg-black" style="width:30px; height:20px;"></div>
+    </div>
+    <div class="flex" style="height:100px;">
+      <!-- <div id="progress-14" class="bg-black mt-auto" style="width:160px; height:10px;"></div> -->
+      <div id="progress-21" class="bg-blue-400" style="width: 20px; height:20px;"></div>
+      <div id="progress-20" class="bg-black" style="width:20px; height:20px;"></div>
+      <div id="progress-19" class="bg-blue-400" style="width:20px; height:20px;"></div>
+      <div id="progress-18" class="bg-black" style="width: 20px; height: 20px;"></div>
+      <div id="progress-17" class="bg-blue-400" style="width:20px; height: 20px;"></div>
+      <div id="progress-16" class="bg-black" style="width:20px; height:20px;"></div>
+      <div id="progress-15" class="bg-blue-400" style="width:20px; height:20px;"></div>
+      <div id="progress-14" class="bg-black" style="width:20px; height:20px;"></div>
+      <div id="progress-13" class="bg-blue-400" style="width:20px; height:20px;"></div>
+      <div id="progress-12" class="bg-black" style="width:20px; height:20px;"></div>
+      <div id="progress-11" class="bg-blue-400" style="width:20px; height:20px;"></div>
+      <div id="progress-10" class="bg-black" style="width:20px; height:20px"></div>
+      <div id="progress-9" class="bg-blue-400" style="width:25px; height:100px;"></div>
+    </div>
+    <div class="flex" style="height:50px;">
+      <div id="progress-8" class="bg-blue-400" style="width:30px; height:50px; margin-left:40px;"></div>
+      <div id="progress-7" class="bg-black" style="width:80px; height:20px"></div>
+      <div id="progress-5" class="bg-black" style="width:115px;  height:20px;"></div>
+    </div>
+    <div class="flex" style="height:60px;">
+      <div id="progress-2" class="bg-blue-400" style="width:30px; height:20px; margin-left: 40px;"></div>
+      <div id="progress-3" class="bg-black" style="width:50px; height:20px;"></div>
+      <div id="progress-4" class="bg-grey-700" style="width:65px; height:20px"></div>
+      <div id="progress-49" class="bg-grey-700" style="width:20px; height:60px"></div>
+    </div>
+    <div class="flex" style="height:130px;">
+      <div id="progress-14" class="bg-black" style="width:20px; height:130px; margin-left: 125px;"></div>
+      <div id="progress-0" class="bg-blue-400" style="width:60px; height:20px;"></div>
+    </div>
+    <div class="flex" style="height:90px;">
+      <div id="escaped" class="bg-blue-400" style="width:20px; height:50px; margin-left: 125px;"></div>
+    </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: "LevelFour",
+  methods:{
+    nextLevel(){
+      this.$store.dispatch('updateLevel');
+    },
+    trackMouse($event){
+      if ($event.target.id == 'lava' || $event.target.id == 'lava-out') {
+
+        console.log('reset the game');
+
+      } else if ($event.target.id == 'escaped') {
+        this.nextLevel();
+      }
+      console.log($event.target.id)
+    }
+  },
+  computed: {
+    ...mapState(['score', 'level'])
+  },
+  mounted(){
+
+  }
+
+};
+</script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,26 +4,33 @@
     <LevelOne v-if="level === 1" />
     <LevelTwo v-if="level === 2" />
     <LevelThree v-if="level === 3" />
-    <GameComplete v-if="level === 4" />
+    <LevelFour v-if="level === 4" />
+    <GameComplete v-if="level === 5" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import {mapState} from 'vuex';
+import {mapState} from "vuex";
 import Welcome from "@/components/Welcome.vue";
 import LevelOne from "@/components/LevelOne.vue";
 import LevelTwo from "@/components/LevelTwo.vue";
 import LevelThree from "@/components/LevelThree.vue";
+import LevelFour from "@/components/LevelFour.vue";
 import GameComplete from "@/components/GameComplete.vue";
 
 export default {
   name: "Game",
   components: {
-    Welcome, LevelOne, LevelTwo, LevelThree, GameComplete
+    Welcome,
+    LevelOne,
+    LevelTwo,
+    LevelThree,
+    LevelFour,
+    GameComplete
   },
   computed: {
-    ...mapState(['score', 'level'])
+    ...mapState(["score", "level"])
   },
 };
 </script>

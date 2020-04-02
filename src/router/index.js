3554c253d72a11de0960d8cfe404194759967d76
Luke Joseph <luke@trackwire.io>
2020-04-02 11:36:56 +0200

    add not found component catch all for 404 errors

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,13 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "*",
+    component: () =>
+      import(
+        /* webpackChunkName: "NotFoundComponent" */ "../views/NotFoundComponent.vue"
+      )
   }
 ];
 

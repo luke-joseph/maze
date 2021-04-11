4aa7b02666788f5510293b929dd80c4c7288b029
Luke Joseph <luke@trackwire.io>
2021-04-11 20:51:24 +0200

    make correct dist file names

--- dist/js/about.0ad9d62b.js
+++
@@ -1,6 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"text-center about text-grey-300 w-1/2"},[s("h1",{staticClass:"text-3xl font-bold tracking-wide mb-12"},[t._v("About The Scary Maze")]),s("p",{staticClass:"mb-4 text-grey-500 text-xl"},[t._v("This game was made with Vue & Vuex by "),s("a",{staticClass:"text-red-300 underline hover:text-red-400",attrs:{href:"https://lukejoseph.dev"}},[t._v("Luke Joseph")])]),s("p",{staticClass:"mb-4 text-grey-500 text-xl"},[t._v("To see the code for this game visit the "),s("a",{staticClass:"text-red-300 underline hover:text-red-400",attrs:{href:"https://github.com/luke-joseph/maze"}},[t._v("github repo")])])])}],i=s("2877"),l={},n=Object(i["a"])(l,a,r,!1,null,null,null);e["default"]=n.exports}}]);
-<<<<<<< HEAD:dist/js/about.67a38c86.js
-//# sourceMappingURL=about.67a38c86.js.map
-=======
-//# sourceMappingURL=about.0ad9d62b.js.map
->>>>>>> development:dist/js/about.0ad9d62b.js
